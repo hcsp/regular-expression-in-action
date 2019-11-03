8800c8befabd3d09623a9719ce1ac3b986b30230
SuyuZhuang <zhuangsuyu1993@163.com>
2019-11-03 12:03:00 +0800

    完成正则练习

--- src/main/java/com/github/hcsp/regex/LogProcessor.java
+++ src/main/java/com/github/hcsp/regex/LogProcessor.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.regex;
 
+import java.util.regex.Pattern;
+
 public class LogProcessor {
     // 传入日志字符串，将每行开头的时间戳删除
     // 返回删除时间戳后的字符串
@@ -15,7 +17,7 @@ public class LogProcessor {
     // TeamCity server version is 2019.1.1 (build 66192)
     // Collecting changes in 2 VCS roots (22s)
     public static String process(String log) {
-        return null;
+        return log.replaceAll("(^|(?<=\\n))(\\[[\\d-: ]+\\] )","");
     }
 
     public static void main(String[] args) {
