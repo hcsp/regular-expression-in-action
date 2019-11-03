acbb3eaffb737323122fcb13842103e91658b8ea
SuyuZhuang <zhuangsuyu1993@163.com>
2019-11-03 12:05:02 +0800

    去掉不需要的import

--- src/main/java/com/github/hcsp/regex/LogProcessor.java
+++ src/main/java/com/github/hcsp/regex/LogProcessor.java
@@ -1,7 +1,5 @@
 package com.github.hcsp.regex;
 
-import java.util.regex.Pattern;
-
 public class LogProcessor {
     // 传入日志字符串，将每行开头的时间戳删除
     // 返回删除时间戳后的字符串
@@ -17,7 +15,7 @@ public class LogProcessor {
     // TeamCity server version is 2019.1.1 (build 66192)
     // Collecting changes in 2 VCS roots (22s)
     public static String process(String log) {
-        return log.replaceAll("(^|(?<=\\n))(\\[[\\d-: ]+\\] )","");
+        return log.replaceAll("(^|(?<=\\n))(\\[[\\d-: ]+\\] )", "");
     }
 
     public static void main(String[] args) {
