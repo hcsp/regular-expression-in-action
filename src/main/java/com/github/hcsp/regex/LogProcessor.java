6d1fc1809407d3f3f85a31fd60fa67cd1f3d8ba1
Xi Pu <xp19@duke.edu>
2020-03-09 16:36:00 -0400

    finish regex exercise

--- src/main/java/com/github/hcsp/regex/LogProcessor.java
+++ src/main/java/com/github/hcsp/regex/LogProcessor.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.regex;
 
+import java.util.Arrays;
+
 public class LogProcessor {
     // 传入日志字符串，将每行开头的时间戳删除
     // 返回删除时间戳后的字符串
@@ -15,15 +17,17 @@ public class LogProcessor {
     // TeamCity server version is 2019.1.1 (build 66192)
     // Collecting changes in 2 VCS roots (22s)
     public static String process(String log) {
-        return null;
+        return log.replaceAll("(?m)^\\[\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}\\]", "");
     }
 
     public static void main(String[] args) {
         String str =
-                "[2019-08-01 21:24:41] bt3102 (11m:21s)\n"
-                        + "[2019-08-01 21:24:42] TeamCity server version is 2019.1.1 (build 66192)\n"
-                        + "[2019-08-01 21:24:43] Collecting changes in 2 VCS roots (22s)\n";
+                "[2019-12-01 01:24:41] bt3102 (11m:21s)\n"
+                        + "[2019-01-01 00:00:00] TeamCity server version is 2019.1.1 [2019-01-01 00:00:00] (build 66192)\n"
+                        + "[2019-01-31 23:59:59] Collecting changes in 2 VCS roots (22s)\n";
 
-        System.out.println(process(str));
+        String[] result = LogProcessor.process(str).split("\\n");
+        System.out.println(Arrays.toString(result));
+//        System.out.println(process(str));
     }
 }
