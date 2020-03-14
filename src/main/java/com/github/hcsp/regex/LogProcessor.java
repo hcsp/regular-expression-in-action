f3f94a83d45eb90829afca263c17c2764d34facf
JN <153080059@qq.com>
2020-03-14 15:15:33 +0800

    修改

--- src/main/java/com/github/hcsp/regex/LogProcessor.java
+++ src/main/java/com/github/hcsp/regex/LogProcessor.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.regex;
 
+import java.util.regex.Pattern;
+
 public class LogProcessor {
     // 传入日志字符串，将每行开头的时间戳删除
     // 返回删除时间戳后的字符串
@@ -14,8 +16,12 @@ public class LogProcessor {
     // bt3102 (11m:21s)
     // TeamCity server version is 2019.1.1 (build 66192)
     // Collecting changes in 2 VCS roots (22s)
+
+    private static final String regex = "(?m)^\\[\\d{4}-\\d{2}-\\d{2}\\s\\d{2}:\\d{2}:\\d{2}]\\s";
+    private static final Pattern logpattern = Pattern.compile(regex);
+
     public static String process(String log) {
-        return log.replaceAll("(?m)^\\[\\d{4}-\\d{2}-\\d{2}\\s\\d{2}:\\d{2}:\\d{2}]\\s", "");
+        return logpattern.matcher(log).replaceAll("");
     }
 
     public static void main(String[] args) {
