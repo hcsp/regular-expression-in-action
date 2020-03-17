9c661a15d6cef9eea68618e020282987053e0701
richard1230 <huyongjinnuaa@163.com>
2020-03-17 09:42:04 +0800

    regEx

--- src/main/java/com/github/hcsp/regex/PhoneNumberMatcher.java
+++ src/main/java/com/github/hcsp/regex/PhoneNumberMatcher.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.regex;
 
+import java.util.regex.Pattern;
+
 public class PhoneNumberMatcher {
     // 请编写一个函数，判断一个字符串是不是合法的固定电话号码
     // 合法的固定电话号码为：区号-号码
@@ -14,7 +16,16 @@ public class PhoneNumberMatcher {
     // 02134-1234 位数不对
     // 123-45678901 区号必须以0开头
     // 021-1234567 三位区号后面只能跟八位电话号码
+    private static Pattern telephone_pattern = Pattern.compile("0\\d{3}-[1-9]\\d{6,7}|0\\d{2}-[1-9][\\d]{7}");
+
     public static boolean isPhoneNumber(String str) {
-        return false;
+        return telephone_pattern.matcher(str).find();
+    }
+
+    public static void main(String[] args) {
+        String telephone1 = "021-12345678";
+        String telephone2 = "123-45678901";
+        System.out.println(isPhoneNumber(telephone1));
+        System.out.println(isPhoneNumber(telephone2));
     }
 }
