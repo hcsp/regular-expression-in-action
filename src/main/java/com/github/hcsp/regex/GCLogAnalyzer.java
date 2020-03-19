d5f8d90c10077acc2b8232f594d5e772fa7c162a
KisetsuLee <42855253+KisetsuLee@users.noreply.github.com>
2020-03-19 13:47:45 +0800

    正则表达式 (#33)
    正则表达式 (#33)

--- src/main/java/com/github/hcsp/regex/GCLogAnalyzer.java
+++ src/main/java/com/github/hcsp/regex/GCLogAnalyzer.java
@@ -1,7 +1,12 @@
 package com.github.hcsp.regex;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class GCLogAnalyzer {
     // 在本项目的根目录下有一个gc.log文件，是JVM的GC日志
@@ -16,11 +21,36 @@ public class GCLogAnalyzer {
     // user=0.02 sys=0.00, real=0.01 分别代表用户态消耗的时间、系统调用消耗的时间和物理世界真实流逝的时间
     // 请将这些信息解析成一个GCActivity类的实例
     // 如果某行中不包含这些数据，请直接忽略该行
-    public static List<GCActivity> parse(File gcLog) {
+    public static List<GCActivity> parse(File gcLog) throws IOException {
+        final List<String> list = Files.readAllLines(gcLog.toPath());
+        return list.stream().filter(GCLogAnalyzer::isGcInfo)
+                .map(GCLogAnalyzer::compositeGCInfo)
+                .collect(Collectors.toList());
+    }
+
+    private static boolean isGcInfo(String line) {
+        Pattern pattern = Pattern.compile("\\[PSYoungGen: (\\d+)K->(\\d+)K\\((\\d+)K\\).*\\]\\D*(\\d+)K->(\\d+)K\\((\\d+)K\\),.+user=((?:\\d\\.?)\\d+).+sys=((?:\\d\\.?)\\d+).+real=((?:\\d\\.?)\\d+)");
+        return pattern.matcher(line).find();
+    }
+
+    private static GCActivity compositeGCInfo(String line) {
+        Pattern pattern = Pattern.compile("\\[PSYoungGen: (\\d+)K->(\\d+)K\\((\\d+)K\\).*\\]\\D*(\\d+)K->(\\d+)K\\((\\d+)K\\),.+user=((?:\\d\\.?)\\d+).+sys=((?:\\d\\.?)\\d+).+real=((?:\\d\\.?)\\d+)");
+        Matcher matcher = pattern.matcher(line);
+        while (matcher.find()) {
+            return new GCActivity(Integer.parseInt(matcher.group(1)),
+                    Integer.parseInt(matcher.group(2)),
+                    Integer.parseInt(matcher.group(3)),
+                    Integer.parseInt(matcher.group(4)),
+                    Integer.parseInt(matcher.group(5)),
+                    Integer.parseInt(matcher.group(6)),
+                    Double.parseDouble(matcher.group(7)),
+                    Double.parseDouble(matcher.group(8)),
+                    Double.parseDouble(matcher.group(9)));
+        }
         return null;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         List<GCActivity> activities = parse(new File("gc.log"));
         activities.forEach(System.out::println);
     }
