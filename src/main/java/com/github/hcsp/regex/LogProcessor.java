d6834dba2375294f3dba5aa2ff58af669b82c3bc
Bing Qin <qinbing08@gmail.com>
2020-04-07 17:16:41 +0800

    正则表达式练习 (#38)
    正则表达式练习 (#38)

--- src/main/java/com/github/hcsp/regex/LogProcessor.java
+++ src/main/java/com/github/hcsp/regex/LogProcessor.java
@@ -1,6 +1,8 @@
 package com.github.hcsp.regex;
 
 public class LogProcessor {
+
+
     // 传入日志字符串，将每行开头的时间戳删除
     // 返回删除时间戳后的字符串
     // 例如，输入字符串：
@@ -14,8 +16,10 @@ public class LogProcessor {
     // bt3102 (11m:21s)
     // TeamCity server version is 2019.1.1 (build 66192)
     // Collecting changes in 2 VCS roots (22s)
+    private static final String LOG_TIME_REGEX = "(?m)^\\[\\d{4}-\\d{1,2}-\\d{1,2} \\d{2}:\\d{2}:\\d{2}]"; // 分行匹配模式的(?m)记号
+
     public static String process(String log) {
-        return null;
+        return log.replaceAll(LOG_TIME_REGEX, "");
     }
 
     public static void main(String[] args) {
--- src/main/java/com/github/hcsp/regex/PhoneNumberMatcher.java
+++ src/main/java/com/github/hcsp/regex/PhoneNumberMatcher.java
@@ -14,7 +14,9 @@ public class PhoneNumberMatcher {
     // 02134-1234 位数不对
     // 123-45678901 区号必须以0开头
     // 021-1234567 三位区号后面只能跟八位电话号码
+    private static final String PHONE_NUNMBER_REGEX = "^0\\d{2}-[1-9]\\d{7}|0\\d{3}-[1-9]\\d{6,7}";
+
     public static boolean isPhoneNumber(String str) {
-        return false;
+        return str.matches(PHONE_NUNMBER_REGEX);
     }
 }
