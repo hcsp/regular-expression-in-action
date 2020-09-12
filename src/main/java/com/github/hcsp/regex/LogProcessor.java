5b865da204bbfb6479617271e39ae998f0485784
DobbyKim <1175088275@qq.com>
2020-09-13 00:49:35 +0800

    regular-expression-action

--- src/main/java/com/github/hcsp/regex/LogProcessor.java
+++ src/main/java/com/github/hcsp/regex/LogProcessor.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.regex;
 
+
+
 public class LogProcessor {
     // 传入日志字符串，将每行开头的时间戳删除
     // 返回删除时间戳后的字符串
@@ -15,7 +17,13 @@ public class LogProcessor {
     // TeamCity server version is 2019.1.1 (build 66192)
     // Collecting changes in 2 VCS roots (22s)
     public static String process(String log) {
-        return null;
+        String regex = "\\[\\d{4}-\\d{2}-\\d{2}\\s+\\d+\\:\\d+\\:\\d+\\]\\s+";
+        String[] split = log.split("\\n");
+        for (int i = 0; i < split.length; i++) {
+            split[i] = split[i].replaceFirst(regex, "");
+        }
+        String join = String.join("\n", split);
+        return join;
     }
 
     public static void main(String[] args) {
