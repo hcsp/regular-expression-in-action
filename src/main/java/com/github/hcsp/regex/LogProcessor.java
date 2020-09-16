8bfe76d14345314cf8d9ba741f6c91cb1e1e8992
刘雨晨 <liuyuchen@cibfintech.com>
2020-09-16 11:38:55 +0800

    work: regex

--- src/main/java/com/github/hcsp/regex/LogProcessor.java
+++ src/main/java/com/github/hcsp/regex/LogProcessor.java
@@ -1,5 +1,8 @@
 package com.github.hcsp.regex;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class LogProcessor {
     // 传入日志字符串，将每行开头的时间戳删除
     // 返回删除时间戳后的字符串
@@ -15,7 +18,9 @@ public class LogProcessor {
     // TeamCity server version is 2019.1.1 (build 66192)
     // Collecting changes in 2 VCS roots (22s)
     public static String process(String log) {
-        return null;
+        Pattern compile = Pattern.compile("(^|\\n)(\\[\\d{4}-\\d{1,2}-\\d{1,2}\\s+\\d{1,2}:\\d{1,2}:\\d{1,2}])");
+        Matcher m = compile.matcher(log);
+        return m.replaceAll("$1");
     }
 
     public static void main(String[] args) {
