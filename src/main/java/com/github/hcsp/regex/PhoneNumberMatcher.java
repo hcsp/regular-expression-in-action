4ca49bc4b023c1376b0b0550463ff9371a43ff04
Ryan <2433005622@qq.com>
2020-09-19 22:38:30 +0800

    正则表达式练习

--- src/main/java/com/github/hcsp/regex/PhoneNumberMatcher.java
+++ src/main/java/com/github/hcsp/regex/PhoneNumberMatcher.java
@@ -1,5 +1,8 @@
 package com.github.hcsp.regex;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class PhoneNumberMatcher {
     // 请编写一个函数，判断一个字符串是不是合法的固定电话号码
     // 合法的固定电话号码为：区号-号码
@@ -15,6 +18,12 @@ public class PhoneNumberMatcher {
     // 123-45678901 区号必须以0开头
     // 021-1234567 三位区号后面只能跟八位电话号码
     public static boolean isPhoneNumber(String str) {
-        return false;
+        Matcher matcher = getMatcher("([0]\\d{2}-[1-9]\\d{7})|([0]\\d{3}-[1-9]\\d{6,7})", str);
+        return matcher.find();
+    }
+
+    static Matcher getMatcher(String regular, String text) {
+        Pattern pattern = Pattern.compile(regular);
+        return pattern.matcher(text);
     }
 }
