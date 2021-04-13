012d22bc439936a8df367ed1ec21d758fb813b7e
Administrator <tangjiexiong@mv.com>
2021-04-13 09:48:13 +0800

    完成电话和时时间戳的正则

--- src/main/java/com/github/hcsp/regex/LogProcessor.java
+++ src/main/java/com/github/hcsp/regex/LogProcessor.java
@@ -1,6 +1,7 @@
 package com.github.hcsp.regex;
 
 public class LogProcessor {
+    private static final String filterTimestamp = "\\[\\d{4}-\\d{1,2}-\\d{1,2}\\s\\d{1,2}:\\d{1,2}:\\d{1,2}\\]";
     // 传入日志字符串，将每行开头的时间戳删除
     // 返回删除时间戳后的字符串
     // 例如，输入字符串：
@@ -15,7 +16,7 @@ public class LogProcessor {
     // TeamCity server version is 2019.1.1 (build 66192)
     // Collecting changes in 2 VCS roots (22s)
     public static String process(String log) {
-        return null;
+        return log.replaceAll(filterTimestamp,"");
     }
 
     public static void main(String[] args) {
