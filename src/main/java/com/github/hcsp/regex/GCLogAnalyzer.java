d3bc08d548d25e02645c33efce55d59d9bff7c6f
Jerry Wang <1976883731@qq.com>
2021-11-23 00:12:08 +0800

    Regular expression in action test

--- src/main/java/com/github/hcsp/regex/GCLogAnalyzer.java
+++ src/main/java/com/github/hcsp/regex/GCLogAnalyzer.java
@@ -1,9 +1,16 @@
 package com.github.hcsp.regex;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class GCLogAnalyzer {
+    private static final Pattern pattern = Pattern.compile("\\[PSYoungGen: (\\d+)K->(\\d+)K\\((\\d+)K\\)].*\\s(\\d+)K->(\\d+)K\\((\\d+)K\\).*user=(\\d\\.\\d+)(?:\\s?).*sys=(\\d\\.\\d+)(?:\\s?).*real=(\\d\\.\\d+)");
+
     // 在本项目的根目录下有一个gc.log文件，是JVM的GC日志
     // 请从中提取GC活动的信息，每行提取出一个GCActivity对象
     //
@@ -16,11 +23,23 @@ public class GCLogAnalyzer {
     // user=0.02 sys=0.00, real=0.01 分别代表用户态消耗的时间、系统调用消耗的时间和物理世界真实流逝的时间
     // 请将这些信息解析成一个GCActivity类的实例
     // 如果某行中不包含这些数据，请直接忽略该行
-    public static List<GCActivity> parse(File gcLog) {
-        return null;
+    public static List<GCActivity> parse(File gcLog) throws IOException {
+        List<String> lines = Files.readAllLines(gcLog.toPath());
+        return lines.stream().map(pattern::matcher)
+                .filter(Matcher::find)
+                .map(matcher -> new GCActivity(Integer.parseInt(matcher.group(1)),
+                        Integer.parseInt(matcher.group(2)),
+                        Integer.parseInt(matcher.group(3)),
+                        Integer.parseInt(matcher.group(4)),
+                        Integer.parseInt(matcher.group(5)),
+                        Integer.parseInt(matcher.group(6)),
+                        Double.parseDouble(matcher.group(7)),
+                        Double.parseDouble(matcher.group(8)),
+                        Double.parseDouble(matcher.group(9))))
+                .collect(Collectors.toList());
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         List<GCActivity> activities = parse(new File("gc.log"));
         activities.forEach(System.out::println);
     }
--- src/main/java/com/github/hcsp/regex/LogProcessor.java
+++ src/main/java/com/github/hcsp/regex/LogProcessor.java
@@ -1,6 +1,9 @@
 package com.github.hcsp.regex;
 
+import java.util.regex.Pattern;
+
 public class LogProcessor {
+    private static final Pattern pattern = Pattern.compile("^(?:\\[\\d{4}-\\d{2}-\\d{2} (?:\\d{2}:){2}\\d{2}]\\s)");
     // 传入日志字符串，将每行开头的时间戳删除
     // 返回删除时间戳后的字符串
     // 例如，输入字符串：
@@ -15,7 +18,7 @@ public class LogProcessor {
     // TeamCity server version is 2019.1.1 (build 66192)
     // Collecting changes in 2 VCS roots (22s)
     public static String process(String log) {
-        return null;
+       return log.replaceAll("\\[\\d{4}-\\d{2}-\\d{2} (?:\\d{2}:){2}\\d{2}]\\s\\b", "");
     }
 
     public static void main(String[] args) {
